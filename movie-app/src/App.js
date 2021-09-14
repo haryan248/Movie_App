1a10022096d1167e8a8eeeb032a1d2af16b85a8d
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-09-14 21:37:57 +0900

    [add] movie component app.js에 추가

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Movie from "./Movie";
+
+const movies = [
+    {
+        title: "Matrix",
+        poster: "",
+    },
+    {
+        title: "Full metal Jacket",
+        poster: "",
+    },
+    {
+        title: "Oldboy",
+        poster: "",
+    },
+    {
+        title: "Star Wars",
+        poster: "",
+    },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {movies.map((movie, index) => {
+                return <Movie title={movie.title} poster={movie.poster} key={index} />;
+            })}
+        </div>
+    );
 }
 
 export default App;
